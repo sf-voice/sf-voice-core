329a62c8746244a98a73e74ba759ec33645d0efc
zmzlois <lois.z@comcord.vision>
2026-05-12 12:29:19 -0700

    append body to cache body ready

--- apps/ellie_ai/lib/ellie_ai/telnyx/cache_body_reader.ex
+++ apps/ellie_ai/lib/ellie_ai/telnyx/cache_body_reader.ex
@@ -12,11 +12,11 @@ defmodule EllieAi.Telnyx.CacheBodyReader do
   def read_body(conn, opts) do
     case Plug.Conn.read_body(conn, opts) do
       {:ok, body, conn} ->
-        conn = Plug.Conn.assign(conn, :raw_body, [body | conn.assigns[:raw_body] || []])
+        conn = Plug.Conn.assign(conn, :raw_body, (conn.assigns[:raw_body] || []) ++ [body])
         {:ok, body, conn}
 
       {:more, body, conn} ->
-        conn = Plug.Conn.assign(conn, :raw_body, [body | conn.assigns[:raw_body] || []])
+        conn = Plug.Conn.assign(conn, :raw_body, (conn.assigns[:raw_body] || []) ++ [body])
         {:more, body, conn}
 
       {:error, _} = err ->
